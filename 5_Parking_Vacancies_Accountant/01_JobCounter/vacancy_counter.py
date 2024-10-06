d8842e6b8b95f357d72f3b804da58bd2782c0503
ludiemert <lucianadiemert@gmail.com>
2024-10-05 22:08:40 -0300

    add ajusts white pixel count =  project 5_Parking_Vacancies_Accountant

--- 5_Parking_Vacancies_Accountant/01_JobCounter/vacancy_counter.py
+++ 5_Parking_Vacancies_Accountant/01_JobCounter/vacancy_counter.py
@@ -30,15 +30,28 @@ while True:
     kernel = np.ones((3,3),np.int8)  #3 linhas por 3 colunas
     imgDil = cv2.dilate(imgMedian,kernel)   #expandir mais os pixels, morfologia  da dilatacao, usar o kernel da biblioteca  (numpy import numpy as np)
 
+#Criar uma contagem de quantas vagas disponíveis tem
+    vacanciesOpen =  0 #variavel q recebe o valor de 0
 
 #verificar se as coordenadas funcionara
     for x,y,w,h in vacancies:
         vacancy = imgDil[y:y+h,x:x+w]    #medir a intensidade de cada vaga - e recortar a img - aquela area - variavel
-        count = cv2.countNonZero(vacancy)
+        count = cv2.countNonZero(vacancy) #valores baixos nao tem carros
         cv2.putText(img,str(count),(x,y+h-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1)
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #contagem dos pixls brancos
 
+        #considerar acima de 900px tem carro abaixo nao tem carro
+        if count <900:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)  #retangulo verde quando tiver a vaga livre
+            vacanciesOpen +=1 #vagas vazias acrescenta +1
 
+        else: #caso contrario
+            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)  #caso contrario retangulo vermelho
+        #print(vacanciesOpen) #print variavel
+
+        #colocar a informacao de vaga vazia dentro da img
+        cv2.rectangle(img,(90,0),(415,60),(0,255,0),-1) #retangulo preenchido all com verde
+        cv2.putText(img,f'FREE: {vacanciesOpen}/69', (95,45),cv2.FONT_HERSHEY_SIMPLEX,1.5,(255,255,255),5) #text com a ing de qtas vazias
 
     cv2.imshow('video color',img)
     #cv2.imshow('video TH',imgMedian)
